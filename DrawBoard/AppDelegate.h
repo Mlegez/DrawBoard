f0682ef37a2a4b34e5f7f5c0b8ca7c48d956cf56
Ze yang <zeyang@promote.cache-dns.local>
2018-03-12 22:31:32 +0800

    Initial Commit

---
+++ DrawBoard/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DrawBoard
+//
+//  Created by Ze yang on 2018/3/12.
+//  Copyright © 2018年 Ze yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DrawBoard/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DrawBoard
+//
+//  Created by Ze yang on 2018/3/12.
+//  Copyright © 2018年 Ze yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
